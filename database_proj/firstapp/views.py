7f36649068df3f4b12deb567d04415de9c11f243
mayur200 <mayur.pardeshi96@gmail.com>
2020-03-21 22:02:53 +0530

    add all files and project in git

--- database_proj/firstapp/views.py
+++ database_proj/firstapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from firstapp.models import Employee
 
 # Create your views here.
+
+def employee_info_view(request):
+    employees = Employee.objects.all()
+    return  render(request, 'testapp/results.html',{'employees':employees})
---
+++ database_proj/populate.py
@@ -0,0 +1,21 @@
+import os
+import django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','database_proj.settings')
+import django
+django.setup()
+
+
+
+from firstapp.models import *
+from faker import  Faker
+from random import *
+faker = Faker()
+
+def populate(n):
+    for i in range(n):
+        feno = randint(1001, 9999)
+        fename = faker.name()
+        fesal = randint(10000, 200000)
+        feaddr = faker.city()
+        emp_record = Employee.objects.get_or_create(eno=feno, ename=fename, esal=fesal, eaddr=feaddr)
+populate(30)
