4b1a08408aa9c74ae30a4323d4256b53b6c2ed88
mayur200 <mayur.pardeshi96@gmail.com>
2020-04-29 00:25:15 +0530

    completed log out and sql injection issue solved

--- my_blog/my_blog/urls.py
+++ my_blog/my_blog/urls.py
@@ -22,5 +22,7 @@ urlpatterns = [
     url('admin/', admin.site.urls, name='admin'),
     url('register/', views.register_page, name='register'),
     url('login/', views.login_page, name='login'),
+    url('logout/', views.logout_user, name='logout'),
     url('', views.home_page, name='home'),
+
 ]
--- my_blog/sign_up/views.py
+++ my_blog/sign_up/views.py
@@ -6,13 +6,16 @@ import sys
 import logging
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from .forms import CreateUserForms   #Created mannually
 
 logger = logging.getLogger(__name__)
 
 
-
 def register_page(request):
+    if request.user.is_authenticated:   #User could not type user "localhost:/login/" & go to login page once he is logged in already
+        return redirect('home')
+    else:
         # form = UserCreationForm() #Importing base django usercreationform
         form = CreateUserForms()
         if request.method == 'POST':
@@ -27,26 +30,37 @@ def register_page(request):
         context = {'form':form}
         return render(request,'testapp/registration.html',context)
 
-
 def login_page(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-        logger.error("Could not read %s", user)
+    if request.user.is_authenticated:    #User could not type user "localhost:/login/" & go to login page once he is logged in already
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
+            logger.error("Could not read %s", user)
 
 
-        if user is not None:
-            login(request, user)
-            return redirect('home')
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.info(request, 'Username or Password is incorrect')
 
-    context = { }
-    return render(request, 'testapp/login.html', context)
 
+        context = { }
+        return render(request, 'testapp/login.html', context)
+
+
+@login_required(login_url='login')
 def home_page(request):
     # logger.error("request>>>>>>>>>>>>>", request)
 
     return  render(request, 'testapp/home.html')
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
 
 
 
